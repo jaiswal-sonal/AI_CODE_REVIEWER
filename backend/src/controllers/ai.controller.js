65f8c8df557d79e83a65214c73103c8956b5de3b
Sonal Jaiswal <151379956+jaiswal-sonal@users.noreply.github.com>
2026-06-15 05:55:39 -0700

    sy

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -1,13 +1,18 @@
- const aiService = require("../services/ai.service");
+const aiService = require("../services/ai.service");
 
-module.exports.getReview =  async (req,res) =>{
-    const code = req.body.code;
+module.exports.getReview = async (req, res) => {
+    try {
+        const code = req.body.code;
 
-    if(!code){
-        return res.status(400).send("prompt is required");
+        if (!code) {
+            return res.status(400).send("Code is required");
+        }
+
+        const response = await aiService(code);
+        res.send(response);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
     }
-
-    const response = await aiService(code);
-    res.send(response);
 };
- 
